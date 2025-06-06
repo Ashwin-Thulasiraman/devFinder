9523e9fe35ab71e72975d1cce189a82b97deb07b
Ashwin-Thulasiraman <ashwintdav@gmail.com>
2025-06-06 14:02:56 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express=require ('express')
 const app=express()
 
+app.use("/",(req,res,next)=>{
+    // res.json("Hi")
+    console.log("Hello")
+    next()
+},(req,res,next)=>{
+    res.json({wash:"wAsh"})
+})
+
 
 app.listen(5000,()=>{
     console.log("Server up and running")
-})
+}) 
